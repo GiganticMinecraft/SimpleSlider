560479a11a79ef1661fd7a946f04514d7f40c33d
Lucky3028 <lucky3028.0@gmail.com>
2020-10-25 21:26:06 +0900

    Initial commit

---
+++ src/main/kotlin/click/seichi/simpleslider/SimpleSlider.kt
@@ -0,0 +1,13 @@
+package click.seichi.simpleslider
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SimpleSlider : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
