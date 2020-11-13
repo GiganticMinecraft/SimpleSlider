ab48660b69cf7a07ba17aabdad4fe3396247fdc6
Lucky3028 <lucky3028.0@gmail.com>
2020-11-13 20:43:14 +0900

    [Change] hoeコマンドを実行する際にPlayer以外から送信された時のエラメの送信先

--- src/main/kotlin/click/seichi/simpleslider/command/HoeCommand.kt
+++ src/main/kotlin/click/seichi/simpleslider/command/HoeCommand.kt
@@ -1,7 +1,6 @@
 package click.seichi.simpleslider.command
 
 import click.seichi.simpleslider.data.OriginalHoe.getOriginalHoe
-import click.seichi.simpleslider.util.Logger
 import org.bukkit.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
@@ -11,7 +10,7 @@ import org.bukkit.entity.Player
 class HoeCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (sender !is Player) {
-            Logger.info("${ChatColor.RED}このコマンドはゲーム内から実行してください。")
+            sender.sendMessage("${ChatColor.RED}このコマンドはゲーム内から実行してください。")
             return true
         }
 
