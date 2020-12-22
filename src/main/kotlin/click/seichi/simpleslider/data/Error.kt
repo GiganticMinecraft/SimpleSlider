aa4363c20eeb695b37a4cf6d37a1b7bea3d9809a
Lucky3028 <lucky3028.0@gmail.com>
2020-12-22 20:32:51 +0900

    [Fix] エラーメッセージの時制

--- src/main/kotlin/click/seichi/simpleslider/data/Error.kt
+++ src/main/kotlin/click/seichi/simpleslider/data/Error.kt
@@ -1,10 +1,10 @@
 package click.seichi.simpleslider.data
 
 enum class Error(val reason: String) {
-    RegionsDuplicatedException("Where you are standing is protected by two or more regions."),
+    RegionsDuplicatedException("Where you are standing was protected by two or more regions."),
     BlockNotFoundException("Failed to get block at the represented location."),
-    SliderNotFoundInSameRegion("The slider doesn't exist in the same region."),
-    SliderNotFound("The slider doesn't exist."),
+    SliderNotFoundInSameRegion("The slider didn't exist in the same region."),
+    SliderNotFound("The slider didn't exist."),
 }
 
 sealed class Result<out R> {
